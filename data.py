114be247c1bcd7eda1bf73f87353e62b8d2c4f38
Qiang(Andrew) Yu <Andrew_yq@hotmail.com>
2017-04-19 23:47:07 -0700

    Update data.py

--- data.py
+++ data.py
@@ -11,7 +11,9 @@ def load_csv(filename):
 def load_adult_data():
     return load_csv("adult.data")
 
-#TODO: Possibly use different data for training and validation
+# Note: Possibly use different data for training and validation to get a more accurate result, 
+# but remember that in the last part your model will be trained on the full training data
+# load_adult_data() and be tested on a test dataset you don't have access to.
 def load_adult_train_data():
     return load_adult_data()
 
