6e7ced2e31aa63fbbbbab74abce982c69ab9e609
Matthew Catron <mc06579@georgiasouthern.edu>
2018-03-22 10:27:35 -0400

    WIP

--- sgd.py
+++ sgd.py
@@ -1,36 +1,68 @@
 from math import exp
 import random
 
-# TODO: Calculate logistic
-def logistic(x):
-    return 1.
 
-# TODO: Calculate dot product of two lists
+def logistic(x):
+    sigmax = 1/(1+exp(-x))  # Logistic Equation
+    return sigmax
+
+
 def dot(x, y):
     s = 0
+    limit = len(x)
+    for i in range(limit):
+        s += x[i]*y[i]
     return s
 
-# TODO: Calculate prediction based on model
+
 def predict(model, point):
-    return 0
+    dot_product = dot(model, point['features'])
+    prediction = logistic(dot_product)
+    return prediction
+
 
 # TODO: Calculate accuracy of predictions on data
 def accuracy(data, predictions):
     correct = 0
+    length = len(data)
+    # Initialize model
+    model = initialize_model(len(data[0]['features']))
+
+    for i in range(length):
+        prediction = predict(model, data[i])
+
+        # Prediction is true if >= to .5 else false
+        if prediction >= .5:
+            predictions[i] = True
+        else:
+            predictions[i] = False
+
+        guess = predictions[i]  # Algorithm Classification
+        real = data[i]['label']  # Actual Classification
+        # If guess is same as real, correct classification
+        if guess == real:
+            correct += 1
+
     return float(correct)/len(data)
+
 
 # TODO: Update model using learning rate and L2 regularization
 def update(model, point, delta, rate, lam):
+    # rate -> learning rate | lam -> regularization parameter |
     pass
+
 
 def initialize_model(k):
     return [random.gauss(0, 1) for x in range(k)]
 
+
 # TODO: Train model using training data
 def train(data, epochs, rate, lam):
+    # epoch just means examining N data points where N is the number of points in your training data
     model = initialize_model(len(data[0]['features']))
     return model
-        
+
+
 def extract_features(raw):
     data = []
     for r in raw:
@@ -42,12 +74,12 @@ def extract_features(raw):
         features.append(float(r['age'])/100)
         features.append(float(r['education_num'])/20)
         features.append(r['marital'] == 'Married-civ-spouse')
-        #TODO: Add more feature extraction rules here!
+        # TODO: Add more feature extraction rules here!
         point['features'] = features
         data.append(point)
     return data
 
+
 # TODO: Tune your parameters for final submission
 def submission(data):
     return train(data, 1, .01, 0)
-    
--- test.py
+++ test.py
@@ -6,37 +6,47 @@ from data import load_adult_train_data, load_adult_valid_data
 
 class SGDTest(unittest.TestCase):
 
+    @unittest.skip("")
+    # STATUS: Good
     def test_logistic(self):
         self.assertAlmostEqual(logistic(1),  0.7310585786300049)
         self.assertAlmostEqual(logistic(2),  0.8807970779778823)
         self.assertAlmostEqual(logistic(-1),  0.2689414213699951)
 
+    @unittest.skip("")
+    # STATUS: Good
     def test_dot(self):
-        d = dot([1.1,2,3.5], [-1,0.1,.08])
+        d = dot([1.1, 2, 3.5], [-1, 0.1, .08])
         self.assertAlmostEqual(d, -.62)
 
+    @unittest.skip("")
+    # STATUS: Good
     def test_predict(self):
-        model = [1,2,1,0,1]
-        point = {'features':[.4,1,3,.01,.1], 'label': 1}
+        model = [1, 2, 1, 0, 1]
+        point = {'features': [.4, 1, 3, .01, .1], 'label': 1}
         p = predict(model, point)
         self.assertAlmostEqual(p, 0.995929862284)
-        
+
+    # @unittest.skip("")
+    # STATUS:
     def test_accuracy(self):
         data = extract_features(load_adult_train_data())
         a = accuracy(data, [0]*len(data))
         self.assertAlmostEqual(a, 0.751077514754)
 
+    @unittest.skip("")
+    # STATUS:
     def test_submission(self):
         train_data = extract_features(load_adult_train_data())
         valid_data = extract_features(load_adult_valid_data())
         model = submission(train_data)
         predictions = [predict(model, p) for p in train_data]
-        print
-        print
-        print "Training Accuracy:", accuracy(train_data, predictions)
+        print("")
+        print("")
+        print("Training Accuracy:", accuracy(train_data, predictions))
         predictions = [predict(model, p) for p in valid_data]
-        print "Validation Accuracy:", accuracy(valid_data, predictions)
-        print
+        print("Validation Accuracy:", accuracy(valid_data, predictions))
+        print("")
 
 
 if __name__ == '__main__':
